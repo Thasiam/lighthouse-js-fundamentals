ed3d9bb06938c7d5c1d64c5332ad7846d64079b1
Thasia Magalhaes <thasia.oliveira@gmail.com>
2023-01-14 12:43:05 -0500

    add a solution for infinit loop

--- loopingRange.js
+++ loopingRange.js
@@ -1,12 +1,22 @@
 function range(start, end, step) {
   let result = [];
 
-  for (let i = start; i <= end; i = i + step) {
-    result.push(i);
+  if (step > 0) {
+    for (let i = start; i <= end; i = i + step) {
+      result.push(i);
+    }
   }
-
+  
   return result;
 }
-console.log(range(0, 10, 2));
-console.log(range(10, 30, 5));
-console.log(range(-5, 2, 3));
+// console.log(range(0, 10, 2));
+// console.log(range(10, 30, 5));
+// console.log(range(-5, 2, 3));
+
+console.log(range(0, 8, 2));
+console.log(range(-9, 3, 3));
+console.log(range());
+console.log(range(8, 0, 2));
+console.log(range(0, 8, -2));
+
+
