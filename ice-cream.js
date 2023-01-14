8f8da033b60ca5c87f7723e3b6595b2527d55f88
Thasia Magalhaes <thasia.oliveira@gmail.com>
2023-01-14 10:18:44 -0500

    addNewice-cream

---
+++ ice-cream.js
@@ -0,0 +1,14 @@
+// Do not modify these first two lines
+//const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+//console.log(iceCreamFlavours);
+
+// Your code below here...
+
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+const newIceCreamFlavours = [...iceCreamFlavours, "root beer"];
+console.log(newIceCreamFlavours);
+console.log(newIceCreamFlavours[0]);
+console.log(newIceCreamFlavours[5]);
+console.log(newIceCreamFlavours.length);
